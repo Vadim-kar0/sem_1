424cf884152a8461f1eed0235f373761f64fd17f
вадим <funpalka1@gmail.com>
2023-04-08 18:22:28 +0400

    Pervii_seminar

---
+++ 1_semin/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите - числа");
+string text = Console.ReadLine();
+int a = Convert.ToInt32(text);
+string text1 = Console.ReadLine();
+int b = Convert.ToInt32(text1);
+
+if(a * a == b)
+{
+    Console.WriteLine("Число является квадратом");
+}
+else
+{
+    Console.Write("число не является квадратом");
+}
